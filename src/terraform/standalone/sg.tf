8f4414b8dad78b823a5f206b6fe9fe719a9a5f69
Maximiliano Falicoff <mfalicoff@yahoo.com>
2023-11-17 16:00:33 -0500

    moved files

--- src/terraform/standalone/sg.tf
+++ src/terraform/standalone/sg.tf
@@ -1,5 +1,5 @@
 resource "aws_security_group" "standalone" {
-  name        = "flask-orchestrator"
+  name        = "standalone"
   description = "Allow inbound HTTP traffic from the broad internet"
   vpc_id      = data.aws_vpc.default.id
 }
@@ -8,7 +8,7 @@ resource "aws_security_group" "standalone" {
 resource "aws_security_group_rule" "standalone_ingress" {
   type              = "ingress"
   from_port         = 80
-  to_port           = 80
+  to_port           = 3306
   protocol          = "tcp"
   cidr_blocks       = ["0.0.0.0/0"]
   ipv6_cidr_blocks  = ["::/0"]
