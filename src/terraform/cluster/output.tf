497b48220ad5a111e699286f25353a681d879bd1
Maximiliano Falicoff <mfalicoff@yahoo.com>
2023-11-17 22:13:41 -0500

    done cluster

---
+++ src/terraform/cluster/output.tf
@@ -0,0 +1,17 @@
+output "manager-data" {
+  value = {
+    ip_address  = aws_instance.cluster_manager.public_ip
+    public_dns = aws_instance.cluster_manager.public_dns
+  }
+  description = "The public IP and DNS of the manager"
+}
+
+output "worker-data" {
+  value = [
+    for instance in aws_instance.cluster_workers : {
+      ip_address  = instance.public_ip
+      public_dns = instance.public_dns
+    }
+  ]
+  description = "The public IP and DNS of the workers"
+}
