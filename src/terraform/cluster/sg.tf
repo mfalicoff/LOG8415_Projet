956ed8ebf0d8c75e0526a9f891a29143fc0d05c7
Maximiliano Falicoff <mfalicoff@yahoo.com>
2023-11-19 16:12:20 -0500

    can connect to all nodes in cluster

--- src/terraform/cluster/sg.tf
+++ src/terraform/cluster/sg.tf
@@ -48,7 +48,7 @@ resource "aws_security_group_rule" "cluster_ingress" {
   from_port         = 0
   to_port           = 0
   protocol          = "-1"
-  source_security_group_id = aws_security_group.proxy.id
+  cidr_blocks      = ["0.0.0.0/0"]
   security_group_id = aws_security_group.cluster.id
 }
 
