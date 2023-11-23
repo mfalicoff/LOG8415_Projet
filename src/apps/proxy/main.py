c15c5fd976578634af390500e6daaf31dd85ecea
Maximiliano Falicoff <mfalicoff@yahoo.com>
2023-11-23 18:25:11 -0500

    proxy main methods done

--- src/apps/proxy/main.py
+++ src/apps/proxy/main.py
@@ -1,12 +1,19 @@
+import random
 import paramiko
 import ping3
 import pymysql
 from sshtunnel import SSHTunnelForwarder
 from dotenv import load_dotenv
 import os
+from flask import Flask, request, jsonify
+
+app = Flask(__name__)
 
 # Load environment variables from the .env file
 load_dotenv(".env.local")
+
+# Define constants
+MYSQL_DEFAULT_PORT = 3306
 
 # Access environment variables
 proxy_ip = os.getenv("PROXY_PUBLIC_IP")
@@ -17,94 +24,112 @@ worker3_ip = os.getenv("WORKER3_PUBLIC_IP")
 ssh_key_path = os.getenv("SSH_KEY_LOCATION")
 
 # Manager node MySQL information
-manager_mysql_host = manager_ip
-manager_mysql_port = 3306
-manager_mysql_user = "ubuntu"
-manager_mysql_password = "root"
-manager_mysql_db = "sakila"
+MYSQL_USER = "ubuntu"
+MYSQL_PASSWORD = "root"
+MYSQL_DB = "sakila"
 
-def direct_hit():
+all_ips = [manager_ip, worker1_ip, worker2_ip, worker3_ip]
+
+
+def create_mysql_connection(ssh_ip, mysql_host, query):
+    # Creates a MySQL connection by establishing an SSH tunnel and connecting to the MySQL server.
+
+    print(f"Connecting to {ssh_ip} and forwarding to {mysql_host}")
     try:
         with SSHTunnelForwarder(
-                manager_ip,
-                ssh_username="ubuntu",
+                ssh_ip,
+                ssh_username=MYSQL_USER,
                 ssh_pkey=paramiko.RSAKey.from_private_key_file(ssh_key_path),
-                remote_bind_address=(manager_ip, 3306)
+                remote_bind_address=(manager_ip, MYSQL_DEFAULT_PORT)
         ):
             conn = pymysql.connect(
-                host=manager_mysql_host,
-                port=manager_mysql_port,
-                user=manager_mysql_user,
-                password=manager_mysql_password,
-                database=manager_mysql_db,
+                host=manager_ip,
+                port=MYSQL_DEFAULT_PORT,
+                user=MYSQL_USER,
+                password=MYSQL_PASSWORD,
+                database=MYSQL_DB,
             )
             print("Connection successful!")
-
-            # Create a cursor object to interact with the database
             cursor = conn.cursor()
-
-            # Execute the query
-            query = "SHOW FULL TABLES"
             cursor.execute(query)
-
-            # Fetch the results
             results = cursor.fetchall()
 
-            # Display the results
             for result in results:
                 print(result)
-
+            return results
     except pymysql.Error as e:
         print(f"Error: {str(e)}")
 
 
-def random():
-    try:
-        with SSHTunnelForwarder(
-                worker3_ip,
-                ssh_username="ubuntu",
-                ssh_pkey=paramiko.RSAKey.from_private_key_file(ssh_key_path),
-                remote_bind_address=(manager_ip, 3306)
-        ):
-            conn = pymysql.connect(
-                host=manager_mysql_host,
-                port=manager_mysql_port,
-                user=manager_mysql_user,
-                password=manager_mysql_password,
-                database=manager_mysql_db,
-            )
-            print("Connection successful!")
+@app.route('/', methods=['GET'])
+def process_query_get():
+    method_type = request.args.get('method_type')
+    query = request.args.get('query')
+    return execute_query(method_type, query)
 
-            # Create a cursor object to interact with the database
-            cursor = conn.cursor()
 
-            # Execute the query
-            query = "SHOW FULL TABLES"
-            cursor.execute(query)
+@app.route('/', methods=['POST'])
+def process_query_post():
+    method_type = request.args.get('method_type')
+    query = request.args.get('query')
+    return execute_query(method_type, query)
 
-            # Fetch the results
-            results = cursor.fetchall()
 
-            # Display the results
-            for result in results:
-                print(result)
+@app.route('/', methods=['DELETE'])
+def process_query_delete():
+    method_type = request.args.get('method_type')
+    query = request.args.get('query')
+    return execute_query(method_type, query)
 
-    except pymysql.Error as e:
-        print(f"Error: {str(e)}")
 
-def customized_hit():
-    # Measure ping times and forward the request to the node with the least response time
+@app.route('/', methods=['PUT'])
+def process_query_put():
+    method_type = request.args.get('method_type')
+    query = request.args.get('query')
+    return execute_query(method_type, query)
+
+
+def execute_query(method_type, query):
+    if method_type == 'direct':
+        result = direct_mysql_connection(query)
+    elif method_type == 'random':
+        result = random_node(query)
+    elif method_type == 'custom':
+        result = customized_hit(query)
+    else:
+        return jsonify({'error': f'Invalid method type: {method_type}'})
+
+    return jsonify({'result': result})
+
+
+def direct_mysql_connection(query):
+    # Use the direct connection function to execute the query and return the results.
+    result = create_mysql_connection(manager_ip, manager_ip, query)
+    return f'Query: {query} executed with direct_mysql_connection and returned {result}'
+
+
+def random_node(query):
+    # Use the SSH tunnel worker connection function to execute the query and return the results.
+    result = create_mysql_connection(all_ips[random.randrange(0, 3)], manager_ip, query)
+    return f'Query: {query} executed with ssh_tunnel_worker_mysql_connection and returned {result}'
+
+
+def customized_hit(query):
+    # Use the function that selects the best node based on latency to execute the query
+    # and return the results.
     lowest_ping_time = float('inf')
     best_node = None
 
-    ping_manager = ping3.ping(manager_ip, timeout=1)
-    ping1 = ping3.ping(worker1_ip, timeout=1)
-    ping2 = ping3.ping(worker2_ip, timeout=1)
-    ping_3 = ping3.ping(worker3_ip, timeout=1)
-    print(ping_manager, ping1, ping2, ping_3)
+    for worker_ip in all_ips:
+        ping_time = ping3.ping(worker_ip, timeout=1)
+        print(f"Ping time for {worker_ip}: {ping_time}")
+        if ping_time is not None and ping_time < lowest_ping_time:
+            lowest_ping_time = ping_time
+            best_node = worker_ip
+    print(f"The node with the lowest ping time is {best_node}")
+    result = create_mysql_connection(best_node, manager_ip, query)
+    return f'Query: {query} executed with customized_hit and returned {result}'
 
 
 if __name__ == "__main__":
-    direct_hit()
-    random()
-    customized_hit()
+    app.run(debug=True)
