060388b5d52da300acc973df98ef39130ef1e8a1
Maximiliano Falicoff <mfalicoff@yahoo.com>
2023-11-23 18:17:09 -0500

    fix sg

--- src/terraform/cluster/sg.tf
+++ src/terraform/cluster/sg.tf
@@ -48,6 +48,7 @@ resource "aws_security_group_rule" "cluster_ingress" {
   from_port         = 0
   to_port           = 0
   protocol          = "-1"
+  cidr_blocks       = ["0.0.0.0/0"]
   ipv6_cidr_blocks  = ["::/0"]
   security_group_id = aws_security_group.cluster.id
 #  source_security_group_id = aws_security_group.proxy.id
