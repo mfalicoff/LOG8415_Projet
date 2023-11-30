2128a8055a8a224c2e9ddddb6c5ed486b58414ed
Maximiliano Falicoff <mfalicoff@yahoo.com>
2023-11-30 18:22:22 -0500

    adding it to standalone

--- src/terraform/standalone/inventory.tf
+++ src/terraform/standalone/inventory.tf
@@ -4,6 +4,13 @@ resource "local_sensitive_file" "private_key" {
   file_permission   = "0600"
 }
 
+resource "local_file" "ansible_cfg" {
+  content = templatefile("templates/ansible.cfg.tftpl", {
+    inventory = local_file.ansible_inventory.filename
+  })
+  filename = format("%s/%s/%s", abspath(path.root), "ansible", "ansible.cfg")
+}
+
 resource "local_file" "ansible_inventory" {
   content = templatefile("templates/inventory.tftpl", {
     ip_addrs = [aws_instance.standalone.public_ip]
